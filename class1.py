f299885f338b3fa873e4d72ed98e596c3e41fd68
Sanif <sanifkandel12345@gmail.com>
2021-02-17 11:15:50 +0545

    Initial commit

---
+++ class1.py
@@ -0,0 +1,22 @@
+"""a='soft'
+b='warica'
+print(a*5)
+
+a='soft'
+b='warica'
+print(a+b)
+
+a='soft'
+print(a[0:4])
+
+b='sanif'
+print(b[-2:0:-1])
+print(b[:])
+print(b[-1:0:-1])
+print(b[::-1])"""
+
+a='Johnson Paul'
+print(a[:])
+print(a[1:])
+print(a[-5:0:-1])
+print(a[-1:-2])
