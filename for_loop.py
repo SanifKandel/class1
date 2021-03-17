0b7a6f54e2b3b5f3f6c30a8b1d44b5f9fa96a735
Sanif <sanifkandel12345@gmail.com>
2021-03-17 11:13:14 +0545

    for loop

---
+++ for_loop.py
@@ -0,0 +1,39 @@
+"""for i in range (4):
+    print(i)
+else:
+    print("Program is over")"""
+
+"""for i in range (4):
+    if i==2:
+        continue
+    print (i)
+"""
+
+#break and continue statement
+"""for val in "string":
+    if val == "i":
+        break
+    print(val)
+print("End")
+
+for val in "string":
+    if val == "i":
+        continue
+    print(val)
+print("End")
+    """
+
+#WAP to stop when banana come
+"""fruits = ["apple","banana","cherry"]
+for x in fruits:
+    print (x)
+    if x == "banana":
+        break"""
+
+"""fruits = ["apple","banana","cherry"]
+for x in fruits:
+    if x =="banana":
+        continue
+    print (x)
+"""
+
